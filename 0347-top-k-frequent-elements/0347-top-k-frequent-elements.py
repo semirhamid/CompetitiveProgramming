5076877edf5233bb209a49b90ee22e652ac94afb
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2022-10-12 21:34:32 +0300

    Update 0347-top-k-frequent-elements.py

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        empty = dict()
+        repeated = dict()
         for i in nums:
-            if i in empty:
-                empty[i]+=1
+            if i in repeated:
+                repeated[i]+=1
             else:
-                empty[i]=1
-        st = sorted(empty,key=lambda x:empty[x], reverse=True)
+                repeated[i]=1
+        st = sorted(repeated,key=lambda x:repeated[x], reverse=True)
         return st[:k]
         
                     
