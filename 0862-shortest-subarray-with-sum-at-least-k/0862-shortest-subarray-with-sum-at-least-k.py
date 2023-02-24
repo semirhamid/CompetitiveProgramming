da83abb20b0a31e6d8578eb84c90fd92e61dc0f1
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2023-02-25 02:35:32 +0300

    Time: 1308 ms (78.84%), Space: 28.4 MB (72.79%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shortestSubarray(self, A, K):
+        d = collections.deque([[0, 0]])
+        res = float('inf')
+        cur = 0
+        for i, a in enumerate(A):
+            cur += a
+            while d and cur - d[0][1] >= K:
+                res = min(res, i + 1 - d.popleft()[0])
+            while d and cur <= d[-1][1]:
+                d.pop()
+            d.append([i + 1, cur])
+        return res if res < float('inf') else -1
