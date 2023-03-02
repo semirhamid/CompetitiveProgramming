8fd763742b55d5bc358165c05790642c9d9c74e4
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2023-03-03 01:56:29 +0300

    Time: 408 ms (94.07%), Space: 19.5 MB (34.09%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,12 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stock = []
+
+    def next(self, price: int) -> int:
+        count = 1
+        while self.stock and self.stock[-1][0] <= price:
+            count += self.stock.pop()[1]
+        self.stock.append([price, count])
+        return count 
+
